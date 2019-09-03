68905c57a908e34f3af128f98c8fd757cf8406e7
root <admin@vpntech.net>
2019-09-03 13:37:06 -0400

    add server exec script

--- findSocket.sh
+++ findSocket.sh
@@ -1,4 +1,4 @@
 lolliSocket(){
-    lsof | grep lollipop | grep /lollipop-| sed 's/[[:space:]]/ /g'| tr -s ' '| cut -d' ' -f9
+    lsof | grep lollipop | grep /lollipop-| sed 's/[[:space:]]/ /g'| tr -s ' '| cut -d' ' -f9 | head -n1
 }
 echo "export SSH_AUTH_SOCK=\$(lolliSocket)"
